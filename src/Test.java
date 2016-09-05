114fa14c0573b9fc6083b466259c2ef0e104c5d4
jana2102 <janaschaub@141.72.137.66>
2016-09-05 16:25:19 +0200

    Aktualisierung zum Test

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-
-public class Test {
-	
-	// Dies ist ein Test-Commit.
-
- // Github commit test
-
-}
-||||||| merged common ancestors
-
-public class Test {
-<<<<<<< HEAD
-
-	// Dies ist ein Test-Commit.
-=======
- // Github commit test
->>>>>>> branch 'master' of https://github.com/SvenC56/vierpunkt
-}
-=======
 
 public class Test {
 	
@@ -32,4 +12,4 @@ public class Test {
 	// Das ist noch ein Test!
 
 }
->>>>>>> origin/master
+
