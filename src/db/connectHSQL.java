b7fc746a19601bf4026f4288908ae0f090be50cf
Sven <svencieslok@googlemail.com>
2016-09-16 17:46:43 +0200

    Datenbank v2
    Datenbank mit funktionierenden Methoden

--- src/db/connectHSQL.java
+++ src/db/connectHSQL.java
@@ -7,54 +7,60 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class connectHSQL {
-	public connectHSQL() {
+	Connection con = null;
 
+	public connectHSQL() {
 		try {
-			// Treiberklasse laden
 			Class.forName("org.hsqldb.jdbcDriver");
 		} catch (ClassNotFoundException e) {
-			System.err.println("Treiberklasse nicht gefunden!");
 			return;
 		}
-
-		Connection con = null;
+		con = null;
 
 		try {
 			con = DriverManager.getConnection(
-					"jdbc:hsqldb:file:C:\\Users\\Sven.Cieslok\\workspace\\vierpunkt\\Datenbank; shutdown=true", "root",
+					"jdbc:hsqldb:file:C:\\Users\\Sven\\workspace\\vierpunkt\\VierGewinntDB; shutdown=true", "root",
 					"vierpunkt");
-			Statement stmt = con.createStatement();
-
-			// Alle Kunden ausgeben
-			String sql = "SELECT * FROM Customer";
-			ResultSet rs = stmt.executeQuery(sql);
-
-			while (rs.next()) {
-				String id = rs.getString(1);
-				String firstName = rs.getString(2);
-				String lastName = rs.getString(3);
-				System.out.println(id + ", " + firstName + " " + lastName);
-			}
-
-			// Resultset schlieﬂen
-			rs.close();
-
-			// Statement schlieﬂen
-			stmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
 	public static void main(String[] args) {
-		new connectHSQL();
+		connectHSQL connect = new connectHSQL();
+		//connect.selectAll();
+		ResultSet r = connect.executeSQL("Select * From Match");
+		connect.printResult(r);
+	}
+
+	public ResultSet executeSQL(String sql) {
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery(sql);
+			return res;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void printResult(ResultSet r) {
+		try {
+			while (r.next()) {
+				int maxColumns = r.getMetaData().getColumnCount();
+				String print = "";
+				for (int i = 1; i < maxColumns; i++) {
+					print += " ";
+					print += r.getMetaData().getColumnName(i);
+					print += " = ";
+					print += r.getString(i);
+					print += ",";
+				}
+				System.out.println(print);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
