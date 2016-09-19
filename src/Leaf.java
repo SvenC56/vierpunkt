6f0ed5c6c7476b45d3a132cddf80539479c7928c
Jana Lang <jana.lang@me.com>
2016-09-19 16:58:43 +0200

    Alpha Beta initial draft

---
+++ src/Leaf.java
@@ -0,0 +1,21 @@
+
+public class Leaf {
+
+	// Attributes
+	private int value;
+	
+	// Getter
+	public int getValue() {
+		return value;
+	}
+	
+	// Setter
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	// Constructor
+	public Leaf() {
+		int value = 0;
+	}
+}
