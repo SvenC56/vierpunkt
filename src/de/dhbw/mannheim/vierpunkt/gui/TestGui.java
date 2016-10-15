fa7a0061b45cda9ae98154afb34dfe462e5099d4
jana2102 <janaschaub@141.72.141.191>
2016-10-15 02:07:54 +0200

    Zwischenspeicherung, Hintergrundfunktionen noch nicht vollstaendig

--- src/de/dhbw/mannheim/vierpunkt/gui/TestGui.java
+++ src/de/dhbw/mannheim/vierpunkt/gui/TestGui.java
@@ -31,11 +31,13 @@ public class TestGui extends Application {
 	private final int l = 70; 		// Seitenlaenge der Grids - spaeter manuelle Einstellung
 	public int spieler = 1; 		// Spieler 1
 	private double breite = Toolkit.getDefaultToolkit().getScreenSize().width; // Breite des Fensters in Pixeln
+	
 	// Erzeugen der Spielsteine
     public javafx.scene.image.Image image1 = new javafx.scene.image.Image(getClass().getResource("spielstein_orange.png").toExternalForm());
     public javafx.scene.image.Image image2 = new javafx.scene.image.Image(getClass().getResource("spielstein_gruen.png").toExternalForm());
     public javafx.scene.image.Image image3 = new javafx.scene.image.Image(getClass().getResource("spielstein_grau.png").toExternalForm());
-    public javafx.scene.image.Image kuerbis = new javafx.scene.image.Image(getClass().getResource("kuerbis.png").toExternalForm()); 
+    public javafx.scene.image.Image backgroundvariable = new javafx.scene.image.Image(getClass().getResource("Sweets_transparent.png").toExternalForm());
+	public javafx.scene.image.Image kuerbis = new javafx.scene.image.Image(getClass().getResource("kuerbis.png").toExternalForm()); 
     public javafx.scene.image.Image fledermaus = new javafx.scene.image.Image(getClass().getResource("fledermaus.png").toExternalForm()); 
     public javafx.scene.image.Image pizza = new javafx.scene.image.Image(getClass().getResource("pizza.png").toExternalForm());
     public javafx.scene.image.Image burger = new javafx.scene.image.Image(getClass().getResource("burger.png").toExternalForm());
@@ -49,20 +51,27 @@ public class TestGui extends Application {
     public javafx.scene.image.Image food = new javafx.scene.image.Image(getClass().getResource("hintergrund_food.png").toExternalForm());
     public javafx.scene.image.Image halloween = new javafx.scene.image.Image(getClass().getResource("hintergrund_halloween.png").toExternalForm());
     public javafx.scene.image.Image sport = new javafx.scene.image.Image(getClass().getResource("hintergrund_sport.png").toExternalForm());
-    public javafx.scene.image.Image sweets = new javafx.scene.image.Image(getClass().getResource("Sweets_transpartent.png").toExternalForm());
+    public javafx.scene.image.Image sweets = new javafx.scene.image.Image(getClass().getResource("Sweets_transparent.png").toExternalForm());
+    
+   // public javafx.scene.image.Image test = new javafx.scene.image.Image("hintergrund_halloween.png", breite, Toolkit.getDefaultToolkit().getScreenSize().height, true,true);
     
     public Color color = Color.rgb(133, 3, 118);
     
+    public void setBackgroundvariable(javafx.scene.image.Image backgroundvariable) {
+		this.backgroundvariable = backgroundvariable;}
     
-    public void setColor(Color color) {
-		this.color = color;
-	}
+    public void setColor(Color color) {	this.color = color;}
 
 	public void setImage1(javafx.scene.image.Image image1) {this.image1 = image1;}
 
 	public void setImage2(javafx.scene.image.Image image2) {this.image2 = image2;}
 
 	public void setImage3(javafx.scene.image.Image image3) {this.image3 = image3;}
+	
+	public BackgroundImage backgroundImage = new BackgroundImage(backgroundvariable, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+	public Background background = new Background(backgroundImage);
+	
+	
 
 	
 	
@@ -83,6 +92,7 @@ public class TestGui extends Application {
 
 		// Layout Boxen
 		VBox root = new VBox(); 				// aeusserste Box
+		root.setBackground(background);
 		HBox content = new HBox();
 		content.setPrefWidth(breite); 			// content ueber gesamte Bildschirmbreite
 		content.setAlignment(Pos.TOP_CENTER); 	// alle Inhalte werden mittig ausgerichtet
@@ -108,32 +118,9 @@ public class TestGui extends Application {
 		// Unterkategorien fuer "themen"
 		MenuItem menu21 = new MenuItem("Suessigkeiten");
 		MenuItem menu22 = new MenuItem("Halloween");
-		MenuItem menu23 = new MenuItem("PizzaBurger");
+		MenuItem menu23 = new MenuItem("Food");
 		MenuItem menu24 = new MenuItem("Sports");
 		themen.getItems().addAll(menu21, menu22, menu23, menu24);
-		
-		menu21.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent e){
-				setImage1(orange);
-				setImage2(gruen);
-			}
-		});
-		
-		
-		
-		menu23.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent e){
-				setImage1(pizza);
-				setImage2(burger);
-			}
-		});
-		
-		menu24.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent e){
-				setImage1(basketball);
-				setImage2(baseball);
-			}
-		});
 
 		// Unterkategorien fuer "hilfe"
 		MenuItem menu31 = new MenuItem("zu Google");
@@ -362,16 +349,62 @@ public class TestGui extends Application {
 				}
 		});
 		
-		menu22.setOnAction(new EventHandler<ActionEvent>(){
+		menu21.setOnAction(new EventHandler<ActionEvent>(){
 			@Override public void handle(ActionEvent e){
-				setColor(Color.BLACK);
+				setColor(Color.PURPLE);
+				setImage1(orange);
+				setImage2(gruen);
+				setBackgroundvariable(sweets);
 				
-				//-fx-fill: linear-gradient(to bottom right, #660066, #993366);
-				setImage1(kuerbis);
-				setImage2(fledermaus);
+				backgroundImage = new BackgroundImage(backgroundvariable, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+				background = new Background(backgroundImage);
+				root.setBackground(background);
 				createGrids(spielfeld, spielmodus, start);
 			}
 		});
+		
+		menu22.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent e){
+				setColor(Color.BLACK);
+				setImage1(kuerbis);
+				setImage2(fledermaus);
+				
+				//root.setStyle("-fx-background-image: url('hintergrund_halloween.png'); -fx-background-position: center; -fx-background-size: contain;");
+				setBackgroundvariable(halloween);
+				backgroundImage = new BackgroundImage(backgroundvariable, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+				background = new Background(backgroundImage);
+				root.setBackground(background);
+				createGrids(spielfeld, spielmodus, start);
+			}
+		});
+		
+		menu23.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent e){
+				setColor(Color.BLACK);
+				setImage1(pizza);
+				setImage2(burger);
+				setBackgroundvariable(food);
+				backgroundImage = new BackgroundImage(backgroundvariable, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+				background = new Background(backgroundImage);
+				root.setBackground(background);
+				createGrids(spielfeld, spielmodus, start);
+			}
+		});
+		
+		menu24.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent e){
+				setColor(Color.CADETBLUE);
+				setImage1(basketball);
+				setImage2(baseball);
+				setBackgroundvariable(sport);
+				backgroundImage = new BackgroundImage(backgroundvariable, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+				background = new Background(backgroundImage);
+				root.setBackground(background);
+				createGrids(spielfeld, spielmodus, start);
+			}
+		});
+		
+		
 		
 		createGrids(spielfeld, spielmodus, start); // Methodenaufruf
 		
@@ -381,7 +414,6 @@ public class TestGui extends Application {
 		primaryStage.setScene(scene);
 		scene.getStylesheets().add(TestGui.class.getResource("Gui.css").toExternalForm());
 
-		root.setStyle("-fx-background-image: url('" + halloween + "');");
 		
 		primaryStage.show();
 	}
@@ -390,7 +422,8 @@ public class TestGui extends Application {
      *******************************************  SPIELFELD ERZEUGEN METHODE  ********************************************
      ********************************************************************************************************************/
     public void createGrids(final GridPane spielfeld, Slider spielmodus, Button start){
-       spielfeld.getChildren().clear();
+    	
+    	spielfeld.getChildren().clear();
         for(anzahlzeilen=0;anzahlzeilen<spielfeld.getRowConstraints().size(); anzahlzeilen++){
             for(anzahlspalten=0; anzahlspalten<spielfeld.getColumnConstraints().size(); anzahlspalten++){
             
