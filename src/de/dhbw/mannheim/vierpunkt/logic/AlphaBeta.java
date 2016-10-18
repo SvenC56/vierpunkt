4d52583d79bdd9c840f6b9b98e1ba902aa6629d9
jlang94 <jana.lang@me.com>
2016-10-18 23:01:04 +0200

    smaller changes in alpha beta algorithm

--- src/de/dhbw/mannheim/vierpunkt/logic/AlphaBeta.java
+++ src/de/dhbw/mannheim/vierpunkt/logic/AlphaBeta.java
@@ -4,6 +4,7 @@ public class AlphaBeta {
 	
 	private static final int POS_INFINITY = (int) Double.POSITIVE_INFINITY;
 	private static final int NEG_INFINITY = (int) Double.NEGATIVE_INFINITY;
+	static int depth = 6;
 
 	public static int getMiniMaxValue(GameLogic game, int alpha, int beta) {
 		
@@ -11,32 +12,32 @@ public class AlphaBeta {
 		GameLogic game_tmp;
 		int minimax_tmp;	// temporary minimax value
 		int minimax_curr;	// current minimax value
-		int player; // values: player 1 = 1, player 2 = 2;
+		int player = game.getPlayer(); // Anmerkung: Wert muss aus GameLogic übergeben werden! (1 oder 2)
 		
 		// Initialization
-		if (game.getPlayer() == 1) { // true if computer is playing
-			player = 1; // computer playing (player 1)
-		} else {
-			player = 2; // manual playing (player 2)
+		if (player == 1){ // true if our agent is playing
+			minimax_curr = alpha; // maximize output for out agent
 		}
-		if (player == 1)
-			minimax_curr = alpha; // search for optimum alpha value
-		else 
-			minimax_curr = beta; // search for optimum beta value
-
+		else { // true if opponent is playing
+			minimax_curr = beta; // minimize output for opponent
+		}
+		
+		if (depth == 0)
+			return game.evaluate();
+		else {
 			// calculate all possible moves
-			for (int i = 0; i < game.getColumn(); i++) {
+			for (int i = 0; i <= game.getColumn(); i++) {
 				game_tmp = game.copy(); // copy current game situation
 				if (game_tmp.validPosition(i) != -1) {
 						minimax_tmp = getMiniMaxValue(game_tmp, alpha, beta);
-						if (player == 1) { // computer playing
+						if (player == 1) { // our agent is playing
 							minimax_curr = java.lang.Math.max(minimax_tmp, minimax_curr);
 							alpha = minimax_curr;
-						}
 							if (alpha >= beta) {
 								return beta;
 							}
-						else { // manual player playing
+						}
+						else { // opponent playing
 							minimax_curr = java.lang.Math.min(minimax_tmp, minimax_curr);
 							beta = minimax_curr;
 							if (beta <= alpha)
@@ -46,19 +47,20 @@ public class AlphaBeta {
 		}				
 		
 		return minimax_curr;
-	
-	}	
+		}
+	}
+		
 	
 	private static int calcMove(GameLogic game) { // method to be evoked by game main
 		
-		int [] values = new int[game.getColumn()];
+		int [] values = new int[game.getColumn()+1];
 		GameLogic game_tmp;
 		
 		for (int i = 0; i <= game.getColumn(); i++)
 			if (game.validPosition(i) != -1) { //position is valid
 				game_tmp = game.copy();
-				game_tmp.setChip(i, 2);
-				values[i] = getMiniMaxValue(game_tmp, NEG_INFINITY, POS_INFINITY);
+				game_tmp.setChip(i, 1);
+				values[i] = getMiniMaxValue(game_tmp, depth, NEG_INFINITY, POS_INFINITY);
 			}
 				int move = -1;
 				int max = NEG_INFINITY;
