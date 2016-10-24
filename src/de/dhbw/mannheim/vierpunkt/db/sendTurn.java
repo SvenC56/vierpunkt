9977760bbc589f603214fc125b1b3cff433cd133
Sven <svencieslok@googlemail.com>
2016-10-24 14:11:55 +0200

    DB Change 3 + Methoden

--- src/de/dhbw/mannheim/vierpunkt/db/sendTurn.java
+++ src/de/dhbw/mannheim/vierpunkt/db/sendTurn.java
@@ -5,8 +5,9 @@ public class sendTurn implements Runnable {
 	int M_ID;
 	String PERSON;
 	int POS_Y;
+	int POS_X;
 
-	public sendTurn(int T_ID, int M_ID, String PERSON, int POS_Y) {
+	public sendTurn(int T_ID, int M_ID, String PERSON, int POS_Y, int POS_X) {
 		this.T_ID = T_ID;
 		this.M_ID = M_ID;
 		this.PERSON = PERSON;
@@ -16,7 +17,7 @@ public class sendTurn implements Runnable {
 	@Override
 	public void run() {
 		connectHSQL dbConnection = new connectHSQL();
-		dbConnection.handOverTurn(T_ID, M_ID, PERSON, POS_Y);
+		dbConnection.handOverTurn(T_ID, M_ID, PERSON, POS_Y, POS_X);
 	}
 
 }
