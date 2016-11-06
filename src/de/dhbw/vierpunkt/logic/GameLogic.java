cf5105f5545def9acf4788ed4fba8fccb2d8776b
Jana Lang <jana.lang@me.com>
2016-11-06 16:01:02 +0100

    insert AI changes into game logic

--- src/de/dhbw/vierpunkt/logic/GameLogic.java
+++ src/de/dhbw/vierpunkt/logic/GameLogic.java
@@ -24,7 +24,7 @@ public class GameLogic {
 	/******************* Attribute ********************************/
 	/**************************************************************/
 	private connectHSQL db = new connectHSQL();
-	private AlphaBeta ki = new AlphaBeta();
+//	private AlphaBeta ki = new AlphaBeta();
 	private TestGui gui = new TestGui();
 	// Spielfeld
 		// MAXIMALE ANZAHL SPALTEN
@@ -44,6 +44,7 @@ public class GameLogic {
 	private String player1 = null;
 	private String player2 = null;
 	private int currentPlayer = 0; //Der aktuelle Spieler
+	private static int depth = 6;
 	
 	// Allgemeine Information: x entspricht Spalte / y entspricht Zeile
 
@@ -145,7 +146,8 @@ public class GameLogic {
 		this.setCurrentPlayer(player);
 		//Maximierung, da eigener Spieler
 		if (getCurrentPlayer() == 2) {
-		x = ki.calcMove(this);
+//		x = ki.calcMove(this);
+		x = calcMove(this, depth);
 		this.setChip(x);
 		this.checkWinner();
 		return x;
@@ -554,5 +556,196 @@ public class GameLogic {
 		}
 		return agentCount2 + 2 * agentCount3 - oppCount2 - 4 * oppCount3;
 	}
+	 
+	 
+	 
+/**
+ * 
+ * Änderungen Jana L.
+ */
 
+	 static int evaluate(GameLogic game) { // bewertet die Spielsituation
+			// System.out.println("Ab jetzt sind wir in evaluate");
+			int agentCount2 = 0;
+			int agentCount3 = 0;
+			int oppCount2 = 0;
+			int oppCount3 = 0;
+			
+			for (int x = 0; x <= game.getColumn(); x++) {
+				for (int y = 0; y <= game.getRow(); y++) {
+					//System.out.println("Jetzt sind wir in den for-Schleifen");
+					//System.out.println("Current Player? " + game.getCurrentPlayer());
+					
+					// new try
+					// in column?
+					if (game.getRow()-y>=1) {
+						if (isRow(game.field, 2, x, y, 0, 1) == 4) {
+							return (int) Double.POSITIVE_INFINITY;
+						}	else if (isRow(game.field, 1, x, y, 0, 1) == 4) {
+							return (int) Double.NEGATIVE_INFINITY;
+						}	else if (isRow(game.field, 2, x, y, 0, 1) == 3) {
+							agentCount3++;
+						}	else if (isRow(game.field, 1, x, y, 0, 1) == 3) {
+							oppCount3++;
+						}	else if (isRow(game.field, 2, x, y, 0, 1) == 2) {
+							agentCount2++;
+						}	else if (isRow(game.field, 1, x, y, 0, 1) == 2) {
+							oppCount2++;
+						}
+					}
+					
+					// in row?
+			        if (game.getColumn()-x>=2) {
+			          if (isRow(game.field,2,x,y,1,0)==4) 
+			            return (int) Double.POSITIVE_INFINITY;  // gewonnen
+			          else if (isRow(game.field,1,x,y,1,0)==4)
+			            return (int) Double.NEGATIVE_INFINITY;  // verloren
+			          // 3 gleiche Chips uebereinander?
+			          else if (isRow(game.field,2,x,y,1,0)==3) 
+			            agentCount3++; 
+			          else if (isRow(game.field,1,x,y,1,0)==3)
+			            oppCount3++;
+			          // 2 gleiche Chips uebereinander?
+			          else if (isRow(game.field,2,x,y,1,0)==2) 
+			            agentCount2++; 
+			          else if (isRow(game.field,1,x,y,1,0)==2)
+			            oppCount2++;
+			        }
+					
+					// in diagonal right?
+					if ((game.getRow()-y>=1) && (game.getColumn()-x>=2)) {
+						if (isRow(game.field,2,x,y,1,1) == 4) {
+				            return (int) Double.POSITIVE_INFINITY; 
+						}	else if (isRow(game.field,1,x,y,1,1) == 4) {
+				            return (int) Double.NEGATIVE_INFINITY;
+						}  else if (isRow(game.field,2,x,y,1,1) == 3) {
+				            agentCount3++; 
+						}	else if (isRow(game.field,1,x,y,1,1) == 3) {
+				            oppCount3++;
+						}	else if (isRow(game.field,2,x,y,1,1) == 2) {
+				            agentCount2++; 
+						}	else if (isRow(game.field,2,x,y,1,1) == 2) {
+				            oppCount2++;
+				          }
+				        } 
+					
+			        // in diagonal left?
+			        if ((game.getColumn()-x>=2) && (y>=3)) {
+			           if (isRow(game.field,2,x,y,1,-1)==4) 
+			            return (int) Double.POSITIVE_INFINITY;  // gewonnen
+			          else if (isRow(game.field,1,x,y,1,-1)==4)
+			            return (int) Double.NEGATIVE_INFINITY;  // verloren
+			          // 3 gleiche Chips uebereinander?
+			          else if (isRow(game.field,2,x,y,1,-1)==3) 
+			            agentCount3++; 
+			          else if (isRow(game.field,1,x,y,1,-1)==3)
+			            oppCount3++;
+			          // 2 gleiche Chips uebereinander?
+			          else if (isRow(game.field,2,x,y,1,-1)==2) 
+			            agentCount2++; 
+			          else if (isRow(game.field,1,x,y,1,-1)==2)
+			            oppCount2++;
+			        } 
+				}
+				}
+				
+				//System.out.println("agentCount2 " + agentCount2 + " agentCount3 " + agentCount3 + " oppCount2 " + oppCount2 + " oppCount3 " + oppCount3);
+				return agentCount2 + 100 * agentCount3 - oppCount2 - 500 * oppCount3;
+				
+			}
+											
+				
+		 private static int isRow(int[][] field, int player, int x, int y, int dx, int dy) {
+			 int cnt = 0;
+			 if (y<=2 && x <= 3) {
+			 if (	((field[y][x] == player) || (field[y][x] == 0)) && ((field[y+1*dy][x+1*dx] == player) || (field[y+1*dy][x+1*dx] == 0))
+				&& ((field[y+2*dy][x+2*dx] == player) || (field[y+2*dy][x+2*dx] == 0)) && ((field[y+3*dy][x+3*dx] == player) || (field[y+3*dy][x+3*dx] == 0))) {
+					
+				 for (int i = 0; i < 4; i++) {
+					 if (field[y+i*dy][x+i*dx] == player) {
+						 cnt++;
+					 }
+				 }
+				}
+			 }
+			 return cnt;
+		 	} 
+		  
+	 
+	 
+	 public static int getAlphaBeta(GameLogic game, int depth, int alpha, int beta) {
+			
+			//Declarations
+			GameLogic game_tmp;
+			int minimax_tmp;	// temporary minimax value
+			int minimax_curr;	// current minimax value
+			
+			// Initialization
+			// System.out.println("Current Player? " + game.getCurrentPlayer());
+			if (game.getCurrentPlayer() == 2){ // true if our agent is playing
+				minimax_curr = alpha; // maximize output for our agent
+			}
+			else { // true if opponent is playing
+				minimax_curr = beta; // minimize output for opponent
+			}
+			
+			if (depth <= 0)
+				return evaluate(game); // evaluate game situation by counting the coins in row/ in diagonal/ in column
+			else {
+				// calculate all possible moves
+				for (int i = 0; i <= game.getColumn(); i++) { // try out all columns
+					game_tmp = game.getDemoGame(); // copy current game situation to simulate possible moves without actually changing the play field
+					if (game_tmp.validPosition(i) != -1) { // position is valid
+							minimax_tmp = getAlphaBeta(game_tmp, depth-1, alpha, beta);
+							if (game.getCurrentPlayer() == 2) { // our agent is playing
+								minimax_curr = Math.max(minimax_tmp, minimax_curr); // is the new value of minimax higher than the old one?
+								alpha = minimax_curr; // highest value becomes alpha
+								if (alpha >= beta) {
+									//System.out.println("beta:" + beta);
+									return beta; // beta equals lower boundary
+								}
+							}
+							else { // opponent playing
+								minimax_curr = Math.min(minimax_tmp, minimax_curr); // is the new value of minimax lower than the old one?
+								beta = minimax_curr; // lowest value becomes beta
+								if (beta <= alpha)
+									//System.out.println("alpha" + alpha);
+									return alpha; // alpha equals upper boundary
+							}			
+				}
+			}				
+			//System.out.println("minimax_curr" + minimax_curr);
+			return minimax_curr;
+			}
+		}
+			
+		
+		public static int calcMove(GameLogic game, int depth) { // method to be evoked by game main to find the best possible move for our agent
+			
+			int [] values = new int[game.getColumn()+1];
+			GameLogic game_tmp;
+			
+			for (int i = 0; i <= game.getColumn(); i++) 
+				if (game.validPosition(i) != -1) { //position is valid
+					game_tmp = game.getDemoGame(); // copy current game situation to simulate moves without actually changing the play field
+					game_tmp.setChip(i); // simulate that our agent is placing coins in column i
+					values[i] = getAlphaBeta(game_tmp, depth, (int) Double.NEGATIVE_INFINITY, (int) Double.POSITIVE_INFINITY);
+				}
+			
+					int move = (int) Double.NEGATIVE_INFINITY;
+					int max = (int) Double.NEGATIVE_INFINITY;
+					for (int j = 0; j <= game.getColumn(); j++) {
+						if ((values[j] > max) && (game.validPosition(j) != -1)) { // position is valid and current value is higher than old value
+							max = values[j]; // currently the highest value
+							move = j; // currently the best move
+						}
+					}
+					System.out.println("move: " + move);
+					System.out.println("max: " + max);
+					return move; // best possible move after all columns have been evaluated
+				
+		}
+	 
+	 
+	 
 }
