320f544ef27ba552dc0e6837db2d0eed99d6e06d
Sven <svencieslok@googlemail.com>
2016-11-07 13:38:37 +0100

    final Fix
    SC

--- src/de/dhbw/vierpunkt/objects/Testing.java
+++ src/de/dhbw/vierpunkt/objects/Testing.java
@@ -2,13 +2,13 @@ package de.dhbw.vierpunkt.objects;
 
 public class Testing {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		Game game = new Game();
 		game.startGame("Tobi", "Sven", 1);
 		game.startMatch();
 		game.playTurn(2);
-		
+		Thread.sleep(3000);
 		
 
 	}
