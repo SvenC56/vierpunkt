e25a0c0334126fd400d271ac1d3409561485ddfe
tobias-jung <tobias@141.72.129.22>
2016-11-10 17:52:10 +0100

    Changes in commander

--- src/de/dhbw/vierpunkt/threadcommander/commander.java
+++ src/de/dhbw/vierpunkt/threadcommander/commander.java
@@ -1,7 +1,6 @@
 package de.dhbw.vierpunkt.threadcommander;
 
 import de.dhbw.vierpunkt.application.GameMainThreads;
-import de.dhbw.vierpunkt.db.sendGame;
 import javafx.application.Application;
 
 public class commander {
